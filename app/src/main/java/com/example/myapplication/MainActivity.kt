6ef88b072415f2e07f03bad8ea5e984eee023157
TriNguyenZombie <tringuyenm@greystonevn.com>
2023-04-18 14:44:39 +0700

    Initial commit

---
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.example.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        // create a function to get max value
+        
+
+
+
+    }
+}
